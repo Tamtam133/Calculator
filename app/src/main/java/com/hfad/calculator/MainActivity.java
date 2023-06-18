b9949efe36973a8f87a80db12f8f8e1c98ddd882
Tamtam <nastyakrut@gmail.com>
2023-06-18 21:17:45 +0500

    Add logic in MainActivity

--- app/src/main/java/com/hfad/calculator/MainActivity.java
+++ app/src/main/java/com/hfad/calculator/MainActivity.java
@@ -3,6 +3,10 @@ package com.hfad.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editTextAnswer = findViewById(R.id.editTextAnswer);
+        Button buttonAnswer = findViewById(R.id.buttonAnswer);
+        TextView textViewCorrectAnswer = findViewById(R.id.textViewCorrectAnswer);
+        TextView textViewIncorrectAnswer = findViewById(R.id.textViewIncorrectAnswer);
+        TextView textViewExample = findViewById(R.id.textViewExample);
+
+        textViewExample.setText("10 + 20 = ?");
+
+        buttonAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editTextAnswer.getText().toString();
+                if (text.equals("30")) {
+                    textViewCorrectAnswer.setVisibility(View.VISIBLE);
+                    textViewIncorrectAnswer.setVisibility(View.GONE);
+                } else {
+                    textViewIncorrectAnswer.setVisibility(View.VISIBLE);
+                    textViewCorrectAnswer.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
